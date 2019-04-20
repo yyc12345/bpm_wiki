6706d153018e90cc3dec08567569133f3d515e65
yyc12345 <yyc12321@outlook.com>
2019-04-20 09:46:36 +0800

    apply some new style

--- docs/en/MAIN.md
+++ docs/en/MAIN.md
@@ -1,8 +1,7 @@
 # Ballance Package Manager Wiki
 
-> **[warning] This website is under construction**
->
-> This website is under construction. Some chapters is empty and other pages have not yet been translated.
+!!! warning "This website is under construction"
+    This website is under construction. Some chapters is empty and other pages have not yet been translated.
 
 ## Brief introduction
 
--- docs/javascript/website.js
+++ docs/javascript/website.js
@@ -7,8 +7,10 @@ function remove_url() {
         if(divCollection[i].classList.contains("md-footer-nav")){
             divCollection[i].parentNode.removeChild(divCollection[i]);
         }
+        /*
         if(divCollection[i].classList.contains("md-footer-copyright")){
             divCollection[i].innerHTML="Powered by <a href=\"https://www.mkdocs.org\">MkDocs</a> and <a href=\"https://squidfunk.github.io/mkdocs-material/\">Material for MkDocs</a>. Text is available under the <a href=\"https://creativecommons.org/licenses/by-nc-sa/4.0/\">CC-BY-NC-SA 4.0</a>.";
         }
+        */
     }
 }
